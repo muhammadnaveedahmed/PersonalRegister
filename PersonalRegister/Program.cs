60ac941b70852e79d1236dd9b07fec400e712cb7
muhammadnaveedahmed <mnahmed786@gmail.com>
2022-05-10 19:29:13 +0200

    Personal Register for One Employee

--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//namespace PersonalRegister
+//{ 
+Console.WriteLine("Enter username:");
+string userName = Console.ReadLine();
+Employee emp = new Employee();
+emp.Name = userName;
+Console.WriteLine("Enter Salary:");
+string userSalary = Console.ReadLine();
+emp.Salary = Convert.ToInt32(userSalary);
+Console.WriteLine("Enter username:" + emp.Name);
+Console.WriteLine("Enter username:" + emp.Salary);
+
+class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+//}
